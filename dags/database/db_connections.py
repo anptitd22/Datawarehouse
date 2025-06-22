ab02f5a80806a5ee9d72ee399b3fdfacbbc30d79
Trần Đức An <146741030+anptitd22@users.noreply.github.com>
2025-06-23 02:35:48 +0700

    Update db_connections.py

--- dags/database/db_connections.py
+++ dags/database/db_connections.py
@@ -8,15 +8,15 @@ def get_pg_conn():
         dbname=conn.schema,
         user=conn.login,
         password=conn.password,
-        host=conn.host,
-        port=conn.port
+        host=conn.host, #host=(<local> or <name_container_docker>)
+        port=conn.port #port=5432
     )
 def get_sqlserver_conn():
     conn = BaseHook.get_connection('sqlserver_default')
     return pyodbc.connect(
-        f"DRIVER=ODBC Driver 17 for SQL Server;"
-        f"SERVER={conn.host},{conn.port};"
-        f"DATABASE={conn.schema};"
+        f"DRIVER=ODBC Driver 17 for SQL Server;" 
+        f"SERVER={conn.host},{conn.port};" #host=host.docker.internal, port=1433
+        f"DATABASE={conn.schema};" 
         f"UID={conn.login};"
         f"PWD={conn.password};"
         f"TrustServerCertificate=yes;"
