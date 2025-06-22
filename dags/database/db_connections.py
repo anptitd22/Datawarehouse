1ffa26eceaf8b7621ee4bef2a74b5d16a4cd841b
Trần Đức An <146741030+anptitd22@users.noreply.github.com>
2025-06-23 02:32:05 +0700

    Update db_connections.py

--- dags/database/db_connections.py
+++ dags/database/db_connections.py
@@ -5,20 +5,20 @@ from airflow.hooks.base import BaseHook
 def get_pg_conn():
     conn = BaseHook.get_connection("postgres_default")
     return psycopg2.connect(
-        dbname="postgres",
-        user="admin",
-        password="123456",
-        host="postgres_container",
-        port="5432"
+        dbname=conn.schema,
+        user=conn.login,
+        password=conn.password,
+        host=conn.host,
+        port=conn.port
     )
 def get_sqlserver_conn():
     conn = BaseHook.get_connection('sqlserver_default')
     return pyodbc.connect(
         f"DRIVER=ODBC Driver 17 for SQL Server;"
-        f"SERVER=host.docker.internal,1433;"
-        f"DATABASE=datamart_db;"
-        f"UID=sa;"
-        f"PWD=an147258;"
-        'TrustServerCertificate=yes;'
-        'Connection Timeout=30;'
+        f"SERVER={conn.host},{conn.port};"
+        f"DATABASE={conn.schema};"
+        f"UID={conn.login};"
+        f"PWD={conn.password};"
+        f"TrustServerCertificate=yes;"
+        f"Connection Timeout=30;"
     )
